7b85c3c824c88f4b2d246d64343d82116fb5e574
Abhilash G Raja <abhilashvarma@gmail.com>
2013-12-20 22:33:02 +0530

    New Test
    Nothing more to extend

---
+++ MVC4SampleWeb/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4SampleWeb.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public double Cost { get; set; }
+    }
+}
